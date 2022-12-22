0a8629591c3ef7fafefbb44539a75cf492819f56
AlexDDK <dhrsmp@gmail.com>
2022-12-22 15:27:41 +0300

    feat: route server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('*', (req, res) => {
+  console.log('111111');
+  res.sendFile(index.html);
+});
+
+
 app.listen(80, () => {
     console.log(`Server has been sterted on port 80`);
 });
