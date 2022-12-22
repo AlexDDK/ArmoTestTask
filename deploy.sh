a3e43bf468febef3ad8a7cfe8abc935ebd5c80f2
AlexDDK <dhrsmp@gmail.com>
2022-12-22 15:11:20 +0300

    feat: add serve

---
+++ deploy.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+
+func_checkStatus(){
+    if [ $? -eq 0 ]
+    then
+        echo "---Command $1 done successfully---"
+    else
+        echo "---fail command $1---"
+        exit 1
+    fi
+}
+
+func_checkNPM(){
+    npm -v
+    if [ $? -eq 0 ]
+    then
+        echo "NPM installed"
+    else    
+        echo "NPM did not install"
+        cd ~
+        curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
+        func_checkStatus "Install node 16.16"
+        sudo bash nodesource_setup.sh
+        sudo apt install nodejs
+        func_checkStatus "install nodejs"
+        sudo apt install npm
+    fi
+}
+
+func_checkGit(){
+    git -v
+    if [ $? -eq 0 ]
+    then
+        echo "Git installed"
+    else   
+        echo "Git did not install"
+        sudo apt install -y git
+        func_checkStatus "install git"
+    fi
+}
+
+
+
+
+echo "---Starting deploy---"
+sudo apt update
+rm -Rf project
+mkdir project
+
+echo "Start installing GIT"
+func_checkGit
+func_checkStatus "Git install"
+
+echo "Start clonning project"
+git clone https://github.com/AlexDDK/ArmoTestTask.git project
+func_checkStatus "Git clone"
+
+
+func_checkNPM
+func_checkStatus "check npm"
+
+cd project
+npm i
+func_checkStatus "install dependencies"
+npm run build
+func_checkStatus "build app"
+cd ..
+
+
+
+
+
+
+
+
+
+
+
+
+
+        sudo apt-get -y update
+        sudo apt-get -y upgrade
+        sudo apt install -y nodejs
+        sudo apt install -y npm
+        func_checkStatus "install npm"
