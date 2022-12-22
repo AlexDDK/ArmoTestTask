e7aa1ea5650f3544793c6f33243f4b085886ef54
AlexDDK <dhrsmp@gmail.com>
2022-12-22 16:54:21 +0300

    fix4

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            publicPath: '/main.bundle.js',
+            publicPath: '/',
             title: 'TestTask',
             template: path.join(__dirname, 'src', 'index.html')
         }),
